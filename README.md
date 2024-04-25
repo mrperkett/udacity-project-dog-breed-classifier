57e363d7fa6b57af2418e53e2e122a3ac8dcf770
Matt Perkett <mperkett@matt-laptop>
2024-04-25 09:41:12 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Introduction
+This is my submission for the *Use a Pre-trained Image Classifier to Identify Dog Breeds* project as part of the Udacity nanodegree [AI Programming with Python](https://learn.udacity.com/nanodegrees/nd089).  The point of this project was to complete the missing implementation and to demonstrate familiarity with PyTorch.
+
+I have left the provided file structure unchanged for ease of review.  It seems that there may be an updated version of this project available (MIT License):
+
+https://github.com/udacity/AIPND-revision
+
+# Setup
+Instructions for setting up and running locally using `pyenv`.
+
+```
+pyenv install 3.11.7
+pyenv virtual-env udacity-dog-classifier 3.11.7
+pyenv local udacity-dog-classifier
+
+git clone XX
+cd udacity-project-dog-breed-classifier/intropyproject-classify-pet-images
+python3 -m pip install -r requirements.txt
+```
+
+# Running
+```
+# running on 40 provided images
+python3 ./check_images.py --dir pet_images --arch alexnet --dogfile dognames.txt > alexnet_pet-images.txt
+python3 ./check_images.py --dir pet_images --arch resnet --dogfile dognames.txt > resnet_pet-images.txt
+python3 ./check_images.py --dir pet_images --arch vgg --dogfile dognames.txt > vgg_pet-images.txt
+
+# running on uploaded images
+python3 ./check_images.py --dir uploaded_images --arch alexnet --dogfile dognames.txt > alexnet_uploaded-images.txt
+python3 ./check_images.py --dir uploaded_images --arch resnet --dogfile dognames.txt > resnet_uploaded-images.txt
+python3 ./check_images.py --dir uploaded_images --arch vgg --dogfile dognames.txt > vgg_uploaded-images.txt
+```
+
+# Results
+## Classifying provided pet images
+Running `check_images.py` on the 40 provided pet images using each of the three model architectures (AlexNet, RESNET, VGG16), yields the results in the table below.
+
+
+| |AlexNet	|RESNET	|VGG16|
+|-|---------|-------|-----|
+|Num images	|40	|40	|40|
+|Num dog images	|30	|30	|30|
+|Num not dog images	|10	|10	|10|
+|Num correctly labeled dog / not dog	|40	|39	|40|
+|Num correctly labeled not dog	|10 (100%)	|9 (90%)	|10 (100%)|
+|Num correctly labeled dog	|30 (100%)	|30 (100%)	|30 (100%)|
+|Num correctly labeled dogs with the correct breed	|24 (80%)	|27 (90%)	|28 (93.3%)|
+|Num exactly matching labels	|30 (75.0%)	|33 (82.5%)	|35 (87.5%)|
+
+## Classifying uploaded images
+The exercise also asked that you upload four images: a dog, the same dog horizontally flipped, a non-dog animal, and an object.  I have included the results in [uploaded_images_classification_summary.pdf](intropyproject-classify-pet-images/uploaded_images_classification_summary.pdf).
---
+++ intropyproject-classify-pet-images/get_pet_labels.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
+#                                                                             
+# PROGRAMMER: 
+# DATE CREATED:                                  
+# REVISED DATE: 
+# PURPOSE: Create the function get_pet_labels that creates the pet labels from 
+#          the image's filename. This function inputs: 
+#           - The Image Folder as image_dir within get_pet_labels function and 
+#             as in_arg.dir for the function call within the main function. 
+#          This function creates and returns the results dictionary as results_dic
+#          within get_pet_labels function and as results within main. 
+#          The results_dic dictionary has a 'key' that's the image filename and
+#          a 'value' that's a list. This list will contain the following item
+#          at index 0 : pet image label (string).
+#
+##
+# Imports python modules
+from os import listdir
+import os.path
+
+# TODO 2: Define get_pet_labels function below please be certain to replace None
+#       in the return statement with results_dic dictionary that you create 
+#       with this function
+# 
+def get_pet_labels(image_dir):
+    """
+    Creates a dictionary of pet labels (results_dic) based upon the filenames 
+    of the image files. These pet image labels are used to check the accuracy 
+    of the labels that are returned by the classifier function, since the 
+    filenames of the images contain the true identity of the pet in the image.
+    Be sure to format the pet labels so that they are in all lower case letters
+    and with leading and trailing whitespace characters stripped from them.
+    (ex. filename = 'Boston_terrier_02259.jpg' Pet label = 'boston terrier')
+    Parameters:
+     image_dir - The (full) path to the folder of images that are to be
+                 classified by the classifier function (string)
+    Returns:
+      results_dic - Dictionary with 'key' as image filename and 'value' as a 
+      List. The list contains for following item:
+         index 0 = pet image label (string)
+    """
+    results_dic = dict()
+    file_names = listdir(image_dir)
+    for file_name in file_names:
+        # string the extension from file_name and split on "_"
+        base_name, _ = os.path.splitext(file_name)
+        split_name = base_name.split("_")
+        
+        # check for format violations
+        if len(split_name) < 2:
+            raise AssertionError(f"file_name ({file_name}) has a different format than expected.  It does not split into 2+ values.")
+        if not split_name[-1].isdigit():
+            raise AssertionError(f"file_name ({file_name}) has a different format than expected. The last split element isn't composed of only digits")
+
+        # create the pet label from the split name (only the last item should be ignored)
+        #   - strip white space
+        #   - set to lowercase
+        #   - delimit multiple words with a space
+        pet_label = " ".join([val.strip().lower() for val in split_name[:-1]])
+
+        # add to dictionary
+        if file_name in results_dic:
+            raise AssertionError(f"Logic error: file_name ({file_name}) is repeated")
+        results_dic[file_name] = [pet_label]
+
+    # Replace None with the results_dic dictionary that you created with this
+    # function
+    return results_dic
